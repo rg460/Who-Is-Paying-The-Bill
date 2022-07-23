09be54cc3f82e1888faca7a3909b6ecd392dc884
Ravi Gordon <84871171+rg460@users.noreply.github.com>
2022-07-23 09:29:46 +0100

    completed solution

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import random
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+#Write your code below this line 👇
+# Plan
+# Generate a random number index between 0 and the length of the list
+random_index = random.randrange(len(names))
+# Get an element from a list based on that index
+random_name = names[random_index]
+# ouput the random name of the person paying the bill 
+print(f"{random_name} is going to buy the meal ")
+  
 
